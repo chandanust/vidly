f88f511636fba22fe7580a9b0565fa55f358bf1c
Chandan Kumar <chandanust@gmail.com>
2020-01-17 19:17:48 +0530

    Refactored index.js

--- index.js
+++ index.js
@@ -1,48 +1,12 @@
-require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
-const error = require('./middleware/error');
-const config = require('config');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const movies = require('./routes/movies');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
-process.on('uncaughtException', (ex) => {
-    console.log('We got an uncaught exception');
-    winston.error(ex.message, ex);
-})
-
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { 
-    db: 'mongodb://localhost/vidly',
-    level: 'error'
- });
-
-if (!config.get('jwtPrivateKey')){
-    console.log('FATAL ERROR: jwtPrivateKey is not defined.');
-    process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost/vidly')
-.then('Connected to mongodb....')
-.catch(err => console.log('Error Occured...', err.message));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+require('./startup/logging');
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
