8901ef6c0f9a6aec155eab888cbff4ef70324174
Chandan Kumar <chandanust@gmail.com>
2020-01-24 19:46:59 +0530

    added returns route using TDD

---
+++ routes/returns.js
@@ -0,0 +1,28 @@
+const { Rental } = require('../models/rental');
+const auth = require('../middleware/auth');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/', auth, async (req, res) => {
+   if(!req.body.customerId) return res.status(400).send('customer id is not provided');
+   if(!req.body.movieId) return res.status(400).send('movie id is not provided');
+   
+   const rental = await Rental.findOne({
+       'customer._id': req.body.customerId,
+       'movie._id': req.body.movieId
+   });
+
+   if(!rental) return res.status(404).send('rental not found');
+
+   if(rental.dateReturned) return res.status(400).send('Return already processed');
+
+   rental.dateReturned = new Date();
+
+   await rental.save();
+
+   return res.status(200).send();
+});
+
+module.exports = router;
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,4 +1,6 @@
+const request = require('supertest');
 const { Rental } = require('../../models/rental');
+const { User } = require('../../models/user');
 const mongoose = require('mongoose');
 
 describe('/api/returns', () =>{
@@ -6,12 +8,21 @@ describe('/api/returns', () =>{
     let customerId;
     let movieId;
     let rental;
+    let token;
 
+    const exec = () =>{
+        return request(server)
+        .post('/api/returns')
+        .set('x-auth-token', token)
+        .send({ customerId, movieId });
+    };
+    
     beforeEach( async () => { 
         server = await require('../../index');
         
         customerId = mongoose.Types.ObjectId();
         movieId = mongoose.Types.ObjectId();
+        token = new User().generateAuthToken();
 
         rental = new Rental({
             customer:{
@@ -33,9 +44,60 @@ describe('/api/returns', () =>{
     });
 
     describe('POST /', () => {
-        it('should work!', async () => {
-            const result = await Rental.findById(rental._id);
-            expect(result).not.toBeNull();
+        it('should return 401 if client is not logged in', async () => {
+            token = '';
+
+            const res = await exec();
+
+            expect(res.status).toBe(401);    
         });
-    })
+
+        it('should return 400 if customerId is not provided', async () => {
+            customerId = '';
+
+            const res = await exec();
+
+            expect(res.status).toBe(400);    
+        });
+
+        it('should return 400 if movieId is not provided', async () => {
+            movieId = '';
+
+            const res = await exec();
+
+            expect(res.status).toBe(400);    
+        });
+
+        it('should return 404 if no rental found', async () => {
+            await Rental.remove({});
+
+            const res = await exec();
+
+            expect(res.status).toBe(404);    
+        });
+
+        it('should return 400 if rental already processed', async () => {
+            rental.dateReturned = new Date();
+            await rental.save();
+
+            const res = await exec();
+
+            expect(res.status).toBe(400);    
+        });
+
+        it('should return 200 if valid request', async () => {
+            const res = await exec();
+
+            expect(res.status).toBe(200);    
+        });
+
+        it('should set the returnDate if input is valid', async () => {
+            const res = await exec();
+
+            const rentalInDb = await Rental.findById(rental._id);
+            const diff = new Date() - rentalInDb.dateReturned;
+            expect(diff).toBeLessThan(10 * 1000);
+        });
+
+    });
 });
