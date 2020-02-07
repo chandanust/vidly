592ab1b0c5a3c84d33bb3aa1fa842f6cec6bbfdd
Chandan Kumar <chandanust@gmail.com>
2020-02-07 19:06:50 +0530

    fixed mongoosewarnings

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,6 @@ const config = require('config')
 
 module.exports = function(){
     const db = config.get('db');
-    mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}`));
+    mongoose.connect(db, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true } )
+    .then(() => winston.info(`Connected to Database`));
 }
